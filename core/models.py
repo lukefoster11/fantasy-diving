b1e131ec5c56b777b2ee7a4e005dfee5677e7bbf
Luke Foster <lukefoster2019@gmail.com>
2022-01-12 17:29:06 -0800

    started views

--- core/models.py
+++ core/models.py
@@ -1,21 +1,29 @@
 from django.db import models
 from django.db.models.query_utils import PathInfo
+from datetime import date
 
 # Create your models here.
 class Meet(models.Model):
     title = models.CharField(max_length=200)
     path = models.CharField(max_length=100)
-    startDate = models.DateField
-    endDate = models.DateField
-    hasResults = models.BooleanField
+    startDate = models.DateField('Start Date', default=date.today)
+    endDate = models.DateField('End Date', default=date.today)
+    hasResults = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
 
 class Event(models.Model):
     meet = models.ForeignKey(Meet, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     entriesPath = models.CharField(max_length=100)
-    date = models.DateField
+    date = models.DateField('Event Date', default=date.today)
 
+    def __str__(self):
+        return self.meet.title + ": " + self.title
+"""
 class Entry(models.Model):
     event = models.ForeignKey(Event, on_delete=models.CASCADE)
     diver = models.CharField(max_length=50)
-    dives = models.JSONField
+    dives = models.JSONField(default=list)
+"""
--- core/views.py
+++ core/views.py
@@ -1,6 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
 from django.http import HttpResponse
+from datetime import date
+from .models import Meet, Event
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the core index.")
+    current_meets = Meet.objects.filter(startDate__lte=date.today()).filter(endDate__gte=date.today())
+    upcoming_meets = Meet.objects.filter(startDate__gt=date.today())
+    context = {'current_meets': current_meets, 'upcoming_meets': upcoming_meets}
+    return render(request, 'core/index.html', context)
